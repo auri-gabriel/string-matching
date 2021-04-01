cafad0a34c8aadedd7b0e16d00fcbb4ee788f1db
Auri Gabriel Castro de Melo <12087669+auri-gabriel@users.noreply.github.com>
2021-04-01 16:47:42 -0300

    Update StringMatching.java

--- stringmatching/src/main/java/com/unipampa/stringmatching/StringMatching.java
+++ stringmatching/src/main/java/com/unipampa/stringmatching/StringMatching.java
@@ -11,7 +11,9 @@ package com.unipampa.stringmatching;
  */
 public class StringMatching {
     public static void main(String[] args) {
-        
+        String teste = "teste";
+        String st = "st";
+        System.out.println(indexOf(teste.toCharArray(), st.toCharArray()));
     }
     
      /**
